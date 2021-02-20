828f21bd8b919b8396d9891858f66bad24f897c5
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2021-02-20 21:14:16 +0000

    Initial Commit

---
+++ Server 2019 Core/scripts/disable-tls.ps1
@@ -0,0 +1,17 @@
+#Disable TLS 1.0
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.0"
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Server"
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Client"
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -Value 0
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "DisabledByDefault" -Value 1
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -Value 0
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "DisabledByDefault" -Value 1
+ 
+#Disable TLS 1.1
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1"
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server"
+new-item -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client"
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -Value 0
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "DisabledByDefault" -Value 1
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -Value 0
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "DisabledByDefault" -Value 1
---
+++ Server 2019 Core/variables.pkr.hcl
@@ -0,0 +1,76 @@
+// variables.pkr.hcl
+variable "vsphere_server" {
+  type    = string
+  default = "smt-lab-vcsa-01.smt-lab.local"
+  description = "vCenter Server"  
+}
+variable "vsphere_user" {
+  type      = string
+  default   = "stephan@smt-lab.local"
+  sensitive = true
+}
+variable "vsphere_password" {}
+variable "insecure_connection" {
+  type    = bool
+  default = true
+}
+variable "vsphere_folder" {
+  type    = string
+  default = "Templates"
+}
+variable "vsphere_datacenter" {}
+variable "vsphere_compute_cluster" {}
+variable "vsphere_portgroup_name" {}
+variable "vsphere_datastore" {}
+
+variable "content_library_destination" {
+  type    = string
+  default = "Images"
+}
+variable "template_library_Name" {
+  type = string
+}
+variable "library_vm_destroy" {
+  type    = bool
+  default = true
+}
+variable "ovf" {
+  type    = bool
+  default = true
+}
+variable "vm_name" {}
+variable "CPUs" {}
+variable "RAM" {}
+variable "disk_size" {}
+variable "ram_reserve_all" {
+  type    = bool
+  default = true
+}
+variable "firmware" {}
+variable "vm_version" {}
+variable "notes" {}
+variable "guest_os_type" {}
+variable "boot_order" {
+  type    = string
+  default = "disk,cdrom"
+}
+variable "communicator" {}
+variable "winrm_user" {}
+variable "winrm_password" {}
+variable "winrm_timeout" {}
+variable "disk_controller_type" {}
+variable "disk_thin_provisioned" {
+  type    = bool
+  default = true
+}
+variable "network_card" {
+  type    = string
+  default = "vmxnet3"
+}
+
+variable "os_iso_path" {}
+variable "vmtools_iso_path" {}
+variable "boot_wait" {}
+variable "boot_command" {}
+variable "config_files" {}
+variable "script_files" {}
