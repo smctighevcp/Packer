150c53071d3d5f956963836b5fbc39ea12380173
smctighevcp <65562694+smctighevcp@users.noreply.github.com>
2021-08-12 16:45:12 +0100

    Update package-installations.ps1

--- Server 2019 Core v2/scripts/package-installations.ps1
+++ Server 2019 Core v2/scripts/package-installations.ps1
@@ -1,6 +1,6 @@
 #Package Installations
 #This file downloads a ZIP file from the Packer Host, extracts the content and installs the applications or agents.
-#There is a simple installation section as well as a section that can be used with commonly packages apps (via the App Deployment Toolkit for example).
+#There is a simple installation section as well as a section that can be used with commonly packaged apps (via the App Deployment Toolkit for example).
 #It then removes all the media from the server.
 
 
